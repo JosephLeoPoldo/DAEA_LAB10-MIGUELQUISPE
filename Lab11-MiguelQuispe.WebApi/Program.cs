548e160c0a306236c413fe0f23e002d5ff8c33b1
Bryan Cama <eledar@gmail.com>
2025-06-22 05:10:34 -0500

    bd-desplegado5

--- Lab11-MiguelQuispe.WebApi/Program.cs
+++ Lab11-MiguelQuispe.WebApi/Program.cs
@@ -3,8 +3,10 @@ using Lab11_MiguelQuispe.Application.Configuration;
 using Lab11_MiguelQuispe.Application.Mapping;
 using Lab11_MiguelQuispe.Domain.Interfaces;
 using Lab11_MiguelQuispe.Infraestructure.configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuración de servicios
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
@@ -14,34 +16,39 @@ builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
+// Middlewares
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
 app.UseHangfireDashboard("/hangfire");
 
+// JOBS programados (ejemplo)
 RecurringJob.AddOrUpdate<IDataCleanupService>(
     "job-limpieza-datos",
     service => service.CleanOldData(),
     Cron.Daily);
 
+// Archivos estáticos
 app.MapStaticAssets();
 
+// Rutas MVC (siguen funcionando, pero la raíz será Swagger)
 app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-if (app.Environment.IsDevelopment())
+
+// SWAGGER COMO PÁGINA PRINCIPAL (en "/")
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticketera API V1");
-        c.RoutePrefix = string.Empty; 
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticketera API V1");
+    c.RoutePrefix = string.Empty; // <- Esto hace que Swagger sea el home
+});
+
 app.Run();
